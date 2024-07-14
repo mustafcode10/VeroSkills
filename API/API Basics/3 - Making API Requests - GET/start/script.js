e2d5d772fc085958275b233461f4bb65d8146e67
mustafcode10 <mustafdiiriye@hotmail.com>
2024-07-14 10:14:07 -0500

    Making API Requests

--- API/API Basics/3 - Making API Requests - GET/start/script.js
+++ API/API Basics/3 - Making API Requests - GET/start/script.js
@@ -1,1 +1,9 @@
 const main = document.querySelector("main");
+
+fetch('https://dog.ceo/api/breeds/image/random')
+.then(res => res.json())
+.then(data => {
+    console.log(data)
+    const img = `<img src="${data.message}" alt="Randon dog breed">`
+    main.insertAdjacentHTML('beforeend',  img)
+})
