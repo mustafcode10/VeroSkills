1d15f9b68b38722d189d67a79bfaca3f370f1f31
mustafcode10 <mustafdiiriye@hotmail.com>
2024-07-14 17:06:44 -0500

    Set up the project and make API requests

--- API/API Basics/13 - Set up the project and make API requests/start/script.js
+++ API/API Basics/13 - Set up the project and make API requests/start/script.js
@@ -2,7 +2,12 @@ const main = document.querySelector("main");
 const apodEl = document.querySelector(".apod");
 const roverList = document.querySelector(".rover-list");
 
-async function fetchData(url) {
+const apiKey = "YOUR API KEY";
+
+const apodURL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+const roverURL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2023-01-15&api_key=${apiKey}`;
+
+async function fetchData(url, handleData) {
   try {
     const response = await fetch(url);
 
@@ -11,8 +16,29 @@ async function fetchData(url) {
     }
 
     const data = await response.json();
-    console.log(data);
+    handleData(data);
   } catch (error) {
     console.error("There is a problem with your fetch:", error);
   }
 }
+
+function handleApodData(data) {
+  const html = `
+    <img src="${data.url}" alt="apod">
+    <p>${data.title}</p>
+  `;
+  apodEl.insertAdjacentHTML("beforeend", html);
+}
+
+function handleRoverData(data) {
+  const photos = data.photos
+    .map(photo => `<li><img src="${photo.img_src}" alt="Rover photo"></li>`)
+    .join("");
+  roverList.insertAdjacentHTML("beforeend", photos);
+}
+
+// Fetch and displat APOD
+fetchData(apodURL, handleApodData);
+
+// Fetch and display rover images
+fetchData(roverURL, handleRoverData);
