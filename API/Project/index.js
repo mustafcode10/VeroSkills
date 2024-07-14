54858ec8ef40773d485f962237526fa8ccde3bee
mustafcode10 <mustafdiiriye@hotmail.com>
2024-07-13 19:54:14 -0500

    fetch api

--- API/Project/index.js
+++ API/Project/index.js
@@ -1,1 +1,9 @@
 console.log('API')
+const main = document.querySelector("main");
+
+fetch("https://dog.ceo/api/breeds/image/random")
+  .then(response => response.json())
+  .then(data => {
+    const img = `<img src="${data.message}" alt="Random dog image">`;
+    main.insertAdjacentHTML("beforeend", img);
+  });
