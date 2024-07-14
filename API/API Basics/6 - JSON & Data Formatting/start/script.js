48a1b40be428ec0755c3b0d862f854f0e3c0f515
mustafcode10 <mustafdiiriye@hotmail.com>
2024-07-14 10:59:12 -0500

    JSON & Data Formatting

--- API/API Basics/6 - JSON & Data Formatting/start/script.js
+++ API/API Basics/6 - JSON & Data Formatting/start/script.js
@@ -1,10 +1,30 @@
 const main = document.querySelector("main");
 
 fetch("https://dog.ceo/api/breed/beagle/images/random/8")
-  .then(response => response.json())
-  .then(data => {
+  .then((response) => response.json())
+  .then((data) => {
     const images = data.message
-      .map(img => `<img src="${img}" alt="Random dog breed">`)
+      .map((img) => `<img src="${img}" alt="Random dog breed">`)
       .join("");
     main.insertAdjacentHTML("beforeend", images);
   });
+
+const jsonObj = {
+  name: "Luna",
+  occupation: "Developer",
+  age: 35,
+  isAwesome: true,
+};
+
+const jsonString = JSON.stringify(jsonObj);
+console.log(jsonString);
+
+const jsonStrigify = `{
+  "name": "Luna",
+  "occupation": "Developer",
+  "age": 35,
+  "isAwesome": true
+}`
+
+const jsonParse = JSON.parse(jsonStrigify)
+console.log(jsonParse)
