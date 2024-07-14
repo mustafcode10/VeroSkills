2634af77e4871038083e4dae67cf1deda300e274
mustafcode10 <mustafdiiriye@hotmail.com>
2024-07-14 09:55:48 -0500

    tools/setup/start

--- API/API Basics/2 - Tools and Setup/start/script.js
+++ API/API Basics/2 - Tools and Setup/start/script.js
@@ -1,1 +1,12 @@
 const main = document.querySelector("main");
+
+
+
+fetch('https://dog.ceo/api/breeds/image/random')
+.then(res => res.json())
+.then(data => {
+    // console.log(data)
+    const image = `<img src="${data.message}" alt"Randon image">`
+    main.insertAdjacentHTML('beforeend', image)
+
+})
