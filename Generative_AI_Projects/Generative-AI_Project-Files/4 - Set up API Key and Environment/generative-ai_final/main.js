da9ac6dfa98bd14f33cf432fc8ceeaf870913e2d
mustafcode10 <mustafdiiriye@hotmail.com>
2024-10-26 19:53:28 -0500

    updated

--- Generative_AI_Projects/Generative-AI_Project-Files/4 - Set up API Key and Environment/generative-ai_final/main.js
+++ Generative_AI_Projects/Generative-AI_Project-Files/4 - Set up API Key and Environment/generative-ai_final/main.js
@@ -1,6 +1,7 @@
 import openai from "./config.js";
 
 async function main() {
+  console.log('hi api', openai)
   const completion = await openai.chat.completions.create({
     messages: [{ role: "user", content: "Tell me a funny joke" }],
     model: "gpt-3.5-turbo",
@@ -10,6 +11,8 @@ async function main() {
 }
 
 main();
+
+
 
 
 
